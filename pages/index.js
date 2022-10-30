4a50e5d1f2d451a0d09ce917a2d8ba28dbc9e8c1
tetpic <rabustes@gmail.com>
2022-10-30 15:23:05 +0300

    add next js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+import Link from "next/link"
+import MainContainer from "../components/MainContainer"
+
+const Index = () => {
+    return (
+        <MainContainer >
+            <div>
+                <h1>
+                    Main page
+                </h1>
+            </div>
+        </MainContainer>
+    )
+}
+
+export default Index
